9672ec2419eb27a54e73b54ce77474972990c2db
Marcos Allan <marcosallanm1to@gmail.com>
2025-04-19 10:15:43 -0300

    Matrix-1

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,7 @@
 package application;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
-
-import entities.Employee;
 
 public class Program {
 
@@ -15,53 +11,32 @@ public class Program {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.print("How many rooms will be rented? ");
 		int n = sc.nextInt();
 		
-		List<Employee> list = new ArrayList<>();
+		int[][] mat = new int[n][n];
 		
-		for (int i = 0; i < n; i++) {
-			System.out.printf("\nEmployee #%d:\n", i + 1);
-			System.out.print("Id: ");
-			int id = sc.nextInt();
-			while (hasId(list, id)) {
-				System.out.println("Id already taken! Try again: ");
-				id = sc.nextInt();
+		for (int i = 0; i < mat.length; i++) {
+			for (int j = 0; j < n; j++) {
+				mat[i][j] = sc.nextInt();
 			}
-			System.out.print("Name: ");
-			sc.nextLine();
-			String name = sc.nextLine();
-			System.out.print("Salary: ");
-			double salary = sc.nextDouble();
-			
-			Employee employee = new Employee(id, name, salary);
-			
-			list.add(employee);
 		}
 		
-		System.out.print("\nEnter the employee id that will have salary increase: ");
-		int idSalary = sc.nextInt();
-		
-		Employee employee = list.stream().filter(x -> x.getId() == idSalary).findFirst().orElse(null);
-		
-		if (employee == null) {
-			System.out.println("This id does not exist!");
-		} else {
-			System.out.print("Enter the percentage: ");
-			double percentage = sc.nextDouble();	
-			employee.setSalary(percentage);
+		System.out.println("Main diagonal:");
+		for (int i = 0; i < mat.length; i++) {
+			System.out.print(mat[i][i] + " ");
 		}
 		
-		System.out.println("\nList of employees:");
-		for (Employee x : list) {
-			System.out.println(x);
+		int count = 0;
+		for (int i = 0; i < mat.length; i++) {
+			for (int j = 0; j < n; j++) {
+				if (mat[i][j] < 0) {
+					count++;
+				}
+			}
 		}
+
+		System.out.printf("\nNegative numbers = %d",  count);
 	
 		sc.close();
-	}
-	
-	public static boolean hasId(List<Employee> list, int id) {
-		Employee emp = list.stream().filter(x -> x.getId() == id).findFirst().orElse(null);
-		return emp != null;
 	}
 }
