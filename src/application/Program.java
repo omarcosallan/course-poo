f957e2a641fdc034b89547486507bfb3629cc7b9
Marcos Allan <marcosallanm1to@gmail.com>
2025-04-24 15:51:07 -0300

    inheritance1

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,63 +1,13 @@
 package application;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
-import java.util.Scanner;
-
-import entities.Client;
-import entities.Order;
-import entities.OrderItem;
-import entities.Product;
-import entities.enums.OrderStatus;
+import entities.BusinessAccount;
 
 public class Program {
 
-	public static void main(String[] args) throws ParseException {
+	public static void main(String[] args) {
 
-		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyy");
-		
-		System.out.println("Enter client data:");
-		System.out.print("Name: ");
-		String clientName = sc.nextLine();
-		System.out.print("E-mail: ");
-		String email = sc.nextLine();
-		System.out.print("Birth date (DD/MM/YYYY): ");
-		Date birthDate = sdf.parse(sc.next());
-		
-		Client client = new Client(clientName, email, birthDate);
-		
-		System.out.println("Enter order data:");
-		System.out.print("Status: ");
-		String status = sc.next();
-		OrderStatus orderStatus = OrderStatus.valueOf(status);
-		
-		System.out.print("How many items to this order? ");
-		int n = sc.nextInt();
-		
+		BusinessAccount account = new BusinessAccount(8010, "Bob Brown", 0.0, 500.0);
 
-		Order order = new Order(new Date(), orderStatus, client);
-		
-		for (int i = 1; i <= n; i++) {
-			System.out.println("Enter #" + i + " item data:");
-			System.out.print("Product name: ");
-			sc.nextLine();
-			String productName = sc.nextLine();
-			System.out.print("Product price: ");
-			double productPrice = sc.nextDouble();
-			System.out.print("Quantity: ");
-			int quantity = sc.nextInt();
-			
-			OrderItem orderItem = new OrderItem(quantity, productPrice, new Product(productName, productPrice));
-			order.addItem(orderItem);
-		}
-		
-		System.out.println("ORDER SUMMARY:");
-		System.out.println(order);
-		
-		sc.close();
+		System.out.println(account.getBalance());
 	}
 }
