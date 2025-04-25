da5d2029ffcd8edb558347343811e70344f236b9
Marcos Allan <marcosallanm1to@gmail.com>
2025-04-25 16:55:09 -0300

    inheritance4

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,23 +1,55 @@
 package application;
 
-import entities.Account;
-import entities.BusinessAccount;
-import entities.SavingsAccount;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Employee;
+import entities.OutsourcedEmployee;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Account acc1 = new Account(1001, "Alex", 1000.0);
-		acc1.withdraw(200.0);
-		System.out.println(acc1.getBalance());
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
 		
-		Account acc2 = new SavingsAccount(1002, "Maria", 1000.0, 0.01);
-		acc2.withdraw(200.0);
-		System.out.println(acc2.getBalance());
+		List<Employee> list = new ArrayList<>();
 		
-		Account acc3 = new BusinessAccount(1003, "Bob", 1000.0, 500.0);
-		acc3.withdraw(200.0);
-		System.out.println(acc3.getBalance());
+		System.out.print("Enter the number of employees: ");
+		int n = sc.nextInt();
+		
+		for (int i = 1; i <= n; i++) {
+			System.out.println("Employee #" + i + " data:");
+			System.out.print("Outsourced (y/n)? ");
+			char ch = sc.next().charAt(0);
+			System.out.print("Name: ");
+			sc.nextLine();
+			String name = sc.nextLine();
+			System.out.print("Hours: ");
+			int hours = sc.nextInt();
+			System.out.print("Value per hour: ");
+			double valuePerHour = sc.nextDouble();
+			
+			if (ch == 'y') {
+				System.out.print("Additional charge: ");
+				double additionalCharge = sc.nextDouble();
+				Employee emp = new OutsourcedEmployee(name, hours, valuePerHour, additionalCharge);
+				list.add(emp);
+			} else {
+				Employee emp = new Employee(name, hours, valuePerHour);
+				list.add(emp);
+			}
+		}
+		
+		System.out.println();
+		System.out.println("PAYMENTS:");
+		
+		for (Employee emp : list) {
+			System.out.println(emp.getName() + " - $ " + String.format("%.2f", emp.payment()));
+		}
+		
+		sc.close();
 	}
 }
