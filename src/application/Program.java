3e8880e8acbe5c3ce2eac854be8f8a50027446bc
Marcos Allan <marcosallanm1to@gmail.com>
2025-04-26 09:30:56 -0300

    inheritance6

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,9 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
 import entities.Account;
 import entities.BusinessAccount;
 import entities.SavingsAccount;
@@ -8,16 +12,20 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		Account acc1 = new Account(1001, "Alex", 1000.0);
-		acc1.withdraw(200.0);
-		System.out.println(acc1.getBalance());
+		Locale.setDefault(Locale.US);
+		List<Account> list = new ArrayList<>();
 		
-		Account acc2 = new SavingsAccount(1002, "Maria", 1000.0, 0.01);
-		acc2.withdraw(200.0);
-		System.out.println(acc2.getBalance());
+		list.add(new SavingsAccount(1001, "Alex", 500.0, 0.01));
+		list.add(new BusinessAccount(1002, "Maria", 1000.0, 400.0));
+		list.add(new SavingsAccount(1003, "Bob", 300.0, 0.01));
+		list.add(new BusinessAccount(1004, "Anna", 500.0, 500.0));
 		
-		Account acc3 = new BusinessAccount(1003, "Bob", 1000.0, 500.0);
-		acc3.withdraw(200.0);
-		System.out.println(acc3.getBalance());
+		double sum = list.stream().mapToDouble(x -> x.getBalance()).sum();
+		System.out.printf("Total balance: %.2f%n", sum);
+		
+		list.stream().forEach(x -> {
+			x.deposit(10);
+			System.out.printf("Updated balance for account %d: %.2f%n", x.getNumber(), x.getBalance());
+		});
 	}
 }
