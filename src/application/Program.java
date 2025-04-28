d9a17d77ba3a22bb919b2f006c0522dd3d230af5
Marcos Allan <marcosallanm1to@gmail.com>
2025-04-28 17:00:56 -0300

    exceptions1 - very bad solution

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,29 +1,51 @@
 package application;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
+
+import model.entities.Reservation;
 
 public class Program {
 
 	public static void main(String[] args) throws ParseException {
+
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
-		File file = new File("C:\\temp\\in.txt");
-		Scanner sc = null;
+		System.out.print("Room number: ");
+		int number = sc.nextInt();
+		System.out.print("Check-in date (dd/MM/yyyy): ");
+		Date checkIn = sdf.parse(sc.next());
+		System.out.print("Check-out date (dd/MM/yyyy): ");
+		Date checkOut = sdf.parse(sc.next());
 		
-		try {
-			sc = new Scanner(file);
-			while (sc.hasNextLine()) {
-				System.out.println(sc.nextLine());
+		if (!checkOut.after(checkIn)) {
+			System.out.println("Error in reservation: Check-out date must be after check-in date");
+		} else {
+			Reservation reservation = new Reservation(number, checkIn, checkOut);
+			System.out.println("Reservation: " + reservation);
+			
+			System.out.println();
+			System.out.println("Enter data to update the reservation:");
+			System.out.print("Check-in date (dd/MM/yyyy): ");
+			checkIn = sdf.parse(sc.next());
+			System.out.print("Check-out date (dd/MM/yyyy): ");
+			checkOut = sdf.parse(sc.next());
+			
+			Date now = new Date();
+			
+			if (checkIn.before(now) || checkOut.before(now)) {
+				System.out.println("Error in reservation: Reservation dates for update must be future dates");
+			} else if (!checkOut.after(checkIn)) {
+				System.out.println("Error in reservation: Check-out date must be after check-in date");
+			} else {
+				reservation.updateDates(checkIn, checkOut);
+				System.out.println("Reservation: " + reservation);
 			}
-		} catch (FileNotFoundException e) {
-			System.out.println("Error opening file: " + e.getMessage());
-		} finally {
-			if (sc != null) {
-				sc.close();
-			}
-			System.out.println("Finally block executed");
 		}
+
+		sc.close();
 	}
 }
