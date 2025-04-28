fd415e746ddb76dc218da75e239153932d34ffe1
Marcos Allan <marcosallanm1to@gmail.com>
2025-04-28 16:08:15 -0300

    exception - finally

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,27 +1,29 @@
 package application;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.text.ParseException;
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Program {
 
 	public static void main(String[] args) throws ParseException {
 		
-		Scanner sc = new Scanner(System.in);
+		File file = new File("C:\\temp\\in.txt");
+		Scanner sc = null;
 		
 		try {
-			String[] vect = sc.nextLine().split(" ");
-			int position = sc.nextInt();
-			System.out.println(vect[position]);
-		} catch (ArrayIndexOutOfBoundsException e) {
-			System.out.println("Invalid position!");
-		} catch (InputMismatchException e) {
-			System.out.println("Input error!");
+			sc = new Scanner(file);
+			while (sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("Error opening file: " + e.getMessage());
+		} finally {
+			if (sc != null) {
+				sc.close();
+			}
+			System.out.println("Finally block executed");
 		}
-		
-		System.out.println("End of program");
-		
-		sc.close();
 	}
 }
