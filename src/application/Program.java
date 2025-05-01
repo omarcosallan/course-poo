fe5ab5bc70d4af1403224d54d224c458f6a0c5e6
Marcos Allan <marcosallanm1to@gmail.com>
2025-05-01 08:55:32 -0300

    try-with-resources

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,30 +9,13 @@ public class Program {
 	public static void main(String[] args) {
 
 		String path = "c:\\temp\\in.txt";
-		FileReader fr = null;
-		BufferedReader br = null;
 
-		try {
-			fr = new FileReader(path);
-			br = new BufferedReader(fr);
-
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
 			while (br.ready()) {
 				System.out.println(br.readLine());
 			}
 		} catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
-		} finally {
-			try {
-				if (fr != null) {
-
-					fr.close();
-				}
-				if (br != null) {
-					br.close();
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
 		}
 	}
 }
