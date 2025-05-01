0b4c90085d66c71230c7cec9862008df2d8b4282
Marcos Allan <marcosallanm1to@gmail.com>
2025-05-01 10:19:35 -0300

    files1

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,23 +1,59 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 import java.util.Scanner;
+
+import entities.Product;
 
 public class Program {
 
 	public static void main(String[] args) {
 
+		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
-		System.out.print("Enter a file path: ");
+
+		List<Product> list = new ArrayList<Product>();
+
+		System.out.print("Enter file path: ");
 		String strPath = sc.nextLine();
-		
-		File path = new File(strPath);
-		
-		System.out.println("getName: " + path.getName());
-		System.out.println("getParent: " + path.getParent());
-		System.out.println("getPath: " + path.getPath());
-		
+
+		File file = new File(strPath);
+		String parentFolder = file.getParent();
+
+		new File(parentFolder + "\\out").mkdir();
+
+		String targetFileStr = parentFolder + "\\out\\summary.csv";
+
+		try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+			while (br.ready()) {
+				String[] line = br.readLine().split(",");
+
+				Product product = new Product(line[0], Double.parseDouble(line[1]), Integer.parseInt(line[2]));
+				list.add(product);
+			}
+
+			try (BufferedWriter bw = new BufferedWriter(new FileWriter(targetFileStr))) {
+				for (Product product : list) {
+					bw.write(product.getName() + "," + String.format("%.2f", product.total()));
+					bw.newLine();
+				}
+
+				System.out.println(targetFileStr + " CREATED!");
+			} catch (IOException e) {
+				System.out.println("Error writing file: " + e.getMessage());
+			}
+		} catch (IOException e) {
+			System.out.println("Error reading file: " + e.getMessage());
+		}
+
 		sc.close();
 	}
 }
