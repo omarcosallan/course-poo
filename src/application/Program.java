78ba9b45cf9adbd89e68c23636f9a573d8b08405
Marcos Allan <marcosallanm1to@gmail.com>
2025-05-05 16:19:47 -0300

    interface - basic project

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,17 +1,12 @@
 package application;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 import java.util.Scanner;
 
-import entities.Product;
+import entities.CarRental;
+import entities.Vehicle;
 
 public class Program {
 
@@ -19,40 +14,18 @@ public class Program {
 
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-
-		List<Product> list = new ArrayList<Product>();
-
-		System.out.print("Enter file path: ");
-		String strPath = sc.nextLine();
-
-		File file = new File(strPath);
-		String parentFolder = file.getParent();
-
-		new File(parentFolder + "\\out").mkdir();
-
-		String targetFileStr = parentFolder + "\\out\\summary.csv";
-
-		try (BufferedReader br = new BufferedReader(new FileReader(file))) {
-			while (br.ready()) {
-				String[] line = br.readLine().split(",");
-
-				Product product = new Product(line[0], Double.parseDouble(line[1]), Integer.parseInt(line[2]));
-				list.add(product);
-			}
-
-			try (BufferedWriter bw = new BufferedWriter(new FileWriter(targetFileStr))) {
-				for (Product product : list) {
-					bw.write(product.getName() + "," + String.format("%.2f", product.total()));
-					bw.newLine();
-				}
-
-				System.out.println(targetFileStr + " CREATED!");
-			} catch (IOException e) {
-				System.out.println("Error writing file: " + e.getMessage());
-			}
-		} catch (IOException e) {
-			System.out.println("Error reading file: " + e.getMessage());
-		}
+		
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+		
+		System.out.println("Entre com os dados do cliente:");
+		System.out.print("Modelo do carro: ");
+		String carModel = sc.nextLine();
+		System.out.print("Retirada (dd/MM/yyyy hh:mm): ");
+		LocalDateTime start = LocalDateTime.parse(sc.nextLine(), dtf);
+		System.out.print("Retorno (dd/MM/yyyy hh:mm): ");
+		LocalDateTime finish = LocalDateTime.parse(sc.nextLine(), dtf);
+		
+		CarRental cr = new CarRental(start, finish, new Vehicle(carModel));
 
 		sc.close();
 	}
