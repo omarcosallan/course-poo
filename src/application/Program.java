d29c4ed601264d005837fbfa4a8a436d2443d173
Marcos Allan <marcosallanm1to@gmail.com>
2025-05-10 08:53:23 -0300

    interface comparable2

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,22 +7,27 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import entities.Employee;
+
 public class Program {
 
 	public static void main(String[] args) {
 
-		List<String> list = new ArrayList<String>();
+		List<Employee> list = new ArrayList<>();
 		String path = "c:\\temp\\in.txt";
 
 		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
 			while (br.ready()) {
-				list.add(br.readLine());
+				String[] fields = br.readLine().split(",");
+				String name = fields[0];
+				double salary = Double.parseDouble(fields[1]);
+				list.add(new Employee(name, salary));
 			}
 
 			Collections.sort(list);
 
-			for (String s : list) {
-				System.out.println(s);
+			for (Employee e : list) {
+				System.out.println(e.getName() + ", " + e.getSalary());
 			}
 		} catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
