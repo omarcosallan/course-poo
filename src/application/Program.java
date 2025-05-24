f054ba9dd77832ed56b366ec518e24b6415c36c7
Marcos Allan <marcosallanm1to@gmail.com>
2025-05-24 09:11:32 -0300

    generics1

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,28 +1,27 @@
 package application;
 
-import java.util.Locale;
 import java.util.Scanner;
 
-import services.BrazilInterestService;
-import services.InterestService;
+import services.PrintService;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.print("Amount: ");
-		double amount = sc.nextDouble();
-		System.out.print("Months: ");
-		int months = sc.nextInt();
+		PrintService<Integer> ps = new PrintService<>();
 		
-		InterestService is = new BrazilInterestService(1.0);
-		double payment = is.payment(amount, months);
+		System.out.print("How many values? ");
+		int n = sc.nextInt();
 		
-		System.out.println("Payment after " + months + " months: ");
-		System.out.println(String.format("%.2f", payment));
+		for (int i = 0; i < n; i++) {
+			Integer value = sc.nextInt();
+			ps.addValue(value);
+		}
+		
+		ps.print();
+		System.out.println("First: " + ps.first());
 		
 		sc.close();
 	}
