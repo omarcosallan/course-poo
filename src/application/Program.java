7286fc14e8ced24185969ccf8807be03146ad6a1
Marcos Allan <marcosallanm1to@gmail.com>
2025-05-26 17:03:23 -0300

    generics5

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,28 +1,34 @@
 package application;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-
-import entities.Circle;
-import entities.Rectangle;
-import entities.Shape;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		List<Shape> myShapes = new ArrayList<>();
-		myShapes.add(new Rectangle(3.0, 2.0));
-		myShapes.add(new Circle(2.0));
+		List<Integer> myInts = Arrays.asList(1, 2, 3, 4);
+		List<Double> myDoubles = Arrays.asList(3.14, 6.28);
+		List<Object> myObjs = new ArrayList<Object>();
 		
-		System.out.println("Total area: " + totalArea(myShapes));
+		copy(myInts, myObjs);
+		printList(myObjs);
+		
+		copy(myDoubles, myObjs);
+		printList(myObjs);
 	}
 
-	public static double totalArea(List<? extends Shape> list) {
-		double sum = 0.0;
-		for (Shape s : list) {
-			sum += s.area();
+	public static void copy(List<? extends Number> source, List<? super Number> destiny) {
+		for (Number number : source) {
+			destiny.add(number);
 		}
-		return sum;
+	}
+	
+	public static void printList(List<?> list) {
+		for (Object obj : list) {
+			System.out.print(obj + " ");
+		}
+		System.out.println();
 	}
 }
