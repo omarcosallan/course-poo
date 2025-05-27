31c33ca82042244b41cb34028f02626509441052
Marcos Allan <marcosallanm1to@gmail.com>
2025-05-27 17:31:50 -0300

    set1

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,17 +1,41 @@
 package application;
 
-import entities.Client;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.Instant;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+import entities.LogEntry;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Client c1 = new Client("Maria", "maria@gmail.com");
-		Client c2 = new Client("Maria", "maria@gmail.com");
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter file full path: ");
+		String path = sc.nextLine();
 		
-		System.out.println(c1.hashCode());
-		System.out.println(c2.hashCode());
-		System.out.println(c1.equals(c2));
-		System.out.println(c1 == c2);
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+			Set<LogEntry> set = new HashSet<>();
+			
+			while (br.ready()) {
+				String line = br.readLine();
+				String[] fields = line.split(" ");
+				String username = fields[0];
+				Date moment = Date.from(Instant.parse(fields[1]));
+				
+				set.add(new LogEntry(username, moment));
+			}
+			
+			System.out.println("Total users: " + set.size());
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+		sc.close();
 	}
 }
